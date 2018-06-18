43468f0ceda4ca5a463f9fd4c49d50048dc493a2
haolang <1363687132@qq.com>
2018-06-18 10:36:50 +0800

    文件添加

---
+++ src/common/time_used.py
@@ -0,0 +1,20 @@
+import time
+
+
+class RunTime:
+
+    def __init__(self):
+        self.start_time = 0
+
+    def start(self):
+        self.start_time = time.time()
+
+    def end(self):
+        end = time.time()
+
+        m, s = divmod(end - self.start_time, 60)
+        h, m = divmod(m, 60)
+        print('%dh, %dm, %ds' % (h, m , s))
+
+
+runtime = RunTime()
