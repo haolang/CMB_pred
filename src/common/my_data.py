3cccf63dff79ddbcc60d34ae51a661a6b530ed12
haolang <1363687132@qq.com>
2018-06-18 10:45:45 +0800

    修改代码

--- src/common/my_data.py
+++ src/common/my_data.py
@@ -1,46 +1,37 @@
 
-class DataInput:
+class Input:
 
     def __init__(self, root_path):
-        self.train_data_path = root_path + r'\input\train.txt'
-        self.stop_data_path = root_path + r'\input\stop.txt'
-        self.test_data_path = root_path + r'\input\test.txt'
-        self.law_data_path = root_path + r'\input\law.txt'
+        self.test_agg = root_path + r'\input\test_agg.csv'
+        self.test_log = root_path + r'\input\test_log.csv'
+        self.train_agg = root_path + r'\input\train_agg.csv'
+        self.train_flg = root_path + r'\input\train_flg.csv'
+        self.train_log = root_path + r'\input\train_log.csv'
 
 
-class DataOutput:
+class Output:
     def __init__(self, root_path):
-        # 初始化训练数据
-        self.f_token_all = root_path + r'\output\f_token_all.csv'
-        self.f_token_content = root_path + r'\output\f_token_content.csv'
-        self.f_token_penalty = root_path + r'\output\f_token_penalty.csv'
-        self.f_token_laws = root_path + r'\output\f_token_laws.csv'
-        # 初始化测试数据
-        self.f_token_test = root_path + r'\output\f_token_test.csv'
+        # 排序后的用户数据
+        self.sorted_train_agg = root_path + r'\output\sorted_train_agg.csv'
+        self.sorted_train_flg = root_path + r'\output\sorted_train_flg.csv'
+        self.sorted_train_log = root_path + r'\output\sorted_train_log.csv'
 
 
 class Model:
     def __init__(self, root_path):
-        self.f_w2v_model = root_path + r'\model\f_w2v_model'
+        pass
+        # self. = root_path + r'\model\'
 
 
 class Feature:
     def __init__(self, root_path):
-        # w2v feature
-        self.f_w2v_feature_train = root_path + r'\feature\f_w2v_feature_train.csv'
-        self.f_w2v_feature_test = root_path + r'\feature\f_w2v_feature_test.csv'
-        # tf-idf
-        self.f_tf_idf_feature = root_path + r'\feature\f_tf_idf_feature.csv'
-        self.f_tf_idf_feature_test = root_path + r'\feature\f_tf_idf_feature_test.csv'
-        # lda feature
-        self.f_lda_feature = root_path + r'\feature\f_lda_feature.csv'
-        self.f_lda_feature_test = root_path + r'\feature\f_lda_feature_test.csv'
-
+        pass
+        # self.*** = root_path + r'\feature\***.csv'
 
 class Data:
     def __init__(self):
-        self.root_path = r'D:\study\big_data\mycode\law_case\data'
-        self.input = DataInput(self.root_path)
-        self.output = DataOutput(self.root_path)
+        self.root_path = r'D:\study\big_data\mycode\CMB_pred\data'
+        self.input = Input(self.root_path)
+        self.output = Output(self.root_path)
         self.model = Model(self.root_path)
         self.feature = Feature(self.root_path)
